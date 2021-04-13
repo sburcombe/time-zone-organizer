d1071a13bae78e1fe8a1fab6c6eedba645105fc7
Stephanie Burcombe <sburcombe@gmail.com>
2021-04-12 17:48:14 -0700

    created dynamic time function to display date and time based on the time zone in the data

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var employees = {
       "id": 1,
       "name": "Stephanie Burcombe",
       "timezone": "PST",
-      "currenttime": "6:18pm",
+      "currenttime": "",
       "workinghours": "8:00am-5:00pm",
       "email": "sburcombe@gmail.com"
     },
@@ -13,7 +13,7 @@ var employees = {
       "id": 2,
       "name": "Dustin Burcombe",
       "timezone": "EST",
-      "currenttime": "9:18pm",
+      "currenttime": "",
       "workinghours": "7:00am-12:00pm",
       "email": "dburcombe@yahoo.com"
     }
@@ -40,8 +40,8 @@ var employeeOneTimeZone = document.querySelector("#time-zone-1");
   var employeeTwoTimeZone = document.querySelector("#time-zone-2");
   employeeTwoTimeZone.innerHTML += employeeTwo["timezone"];
 
-  getCurrentTimes();
   //Display Employee Current Times
+  getCurrentTimes();
 var employeeOneCurrentTime = document.querySelector("#current-time-1");
   employeeOneCurrentTime.innerHTML += employeeOne["currenttime"];
 var employeeTwoCurrentTime = document.querySelector("#current-time-2");
@@ -52,31 +52,32 @@ var employeeOneWorkingHours = document.querySelector("#working-hours-1");
   employeeOneWorkingHours.innerHTML += employeeOne["workinghours"];
 var employeeTwoWorkingHours = document.querySelector("#working-hours-2");
   employeeTwoWorkingHours.innerHTML += employeeTwo["workinghours"];
+
+
 });
 
-
-function getCurrentTimes(){
+function getCurrentTimes() {
   //get Date time and save in specific time zones
   var date = new Date();
-  console.log("date: ", date);
+
   //AST
-   var ASTdate = date.toLocaleString("en-US", {timeZone: "America/Anguilla"});
-   console.log("ast: ", ASTdate);
+  var ASTdate = date.toLocaleString("en-US", { timeZone: "America/Anguilla" });
+
   //EST
   var ESTdate = date.toLocaleString("en-US", { timeZone: "America/Cayman" });
-  console.log("est: ", ESTdate);
+
   //CST
   var CSTdate = date.toLocaleString("en-US", { timeZone: "America/Belize" });
-  console.log("cst: ", CSTdate);
+
   //MST
   var MSTdate = date.toLocaleString("en-US", { timeZone: "America/Phoenix" });
-  console.log("mst: ", MSTdate);
+
   //PST
   var PSTdate = date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
-  console.log("pst: ", PSTdate);
+
   //AKST
   var AKSTdate = date.toLocaleString("en-US", { timeZone: "America/Adak" });
-  console.log("akst: ", AKSTdate);
+
   //HST
 
   //UTC-11 (Samoa standard time)
@@ -84,7 +85,10 @@ function getCurrentTimes(){
   //UTC+10 (Chamorro Standard Time)
 
   if (employeeOne['timezone'] == "PST") {
-    employeeOne["currentTime"] = PSTdate;
+    employeeOne["currenttime"] = PSTdate;
+  }
+  if (employeeTwo['timezone'] == "EST") {
+    employeeTwo["currenttime"] = ESTdate;
   }
 
 }
