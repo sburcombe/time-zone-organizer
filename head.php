121bafded279976f0f72a619d7cc5a6dc068a201
Stephanie Burcombe <sburcombe@gmail.com>
2023-09-18 13:31:13 -0700

    adding footer and trying to add flaticon

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>SoLuna</title>
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link
@@ -12,5 +13,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <!-- <script src="getEmployees.json"></script> -->
+  <script src="https://kit.fontawesome.com/2f351a5043.js" crossorigin="anonymous"></script>
   <script src="script.js"></script>
 </head>
