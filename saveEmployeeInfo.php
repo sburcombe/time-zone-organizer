ea37ceae0b1349b9fb86c99b25cddd11f1e7067e
Stephanie Burcombe <sburcombe@gmail.com>
2023-09-27 13:55:31 -0700

    dynamically add an employee to the json file and dynamically add an id to their object

--- saveEmployeeInfo.php
+++ saveEmployeeInfo.php
@@ -2,7 +2,7 @@
 <?php
 // Append new form data in json string saved in text file
 
-
+var_dump($_POST);
 // path and name of the file
 $filetxt = 'getEmployees.json';
 
@@ -12,12 +12,7 @@ if (isset($_POST['employee-name']) && isset($_POST['time-zone']) && isset($_POST
   if (empty($_POST['employee-name']) || empty($_POST['time-zone']) || empty($_POST['working-hours']) ) {
     echo 'All fields are required';
   } else {
-    // gets and adds form data into an array
-    $formdata = array(
-      'name' => $_POST['employee-name'],
-      'timezone' => $_POST['time-zone'],
-      'workinghours' => $_POST['working-hours'],
-    );
+
 
     // path and name of the file
     $filetxt = 'getEmployees.json';
@@ -30,18 +25,29 @@ if (isset($_POST['employee-name']) && isset($_POST['time-zone']) && isset($_POST
 
       // converts json string into array
       $arr_data = json_decode($jsondata, true);
+
     }
 
+    // gets and adds form data into an array
+    $formdata = array(
+      'id' => count($arr_data['employees']) + 1,
+      'name' => $_POST['employee-name'],
+      'timezone' => $_POST['time-zone'],
+      'workinghours' => $_POST['working-hours'],
+    );
+
     // appends the array with new form data
-    $arr_data[] = $formdata;
+    $arr_data['employees'][] = $formdata;
 
     // encodes the array into a string in JSON format (JSON_PRETTY_PRINT - uses whitespace in json-string, for human readable)
     $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
 
     // saves the json string in "formdata.txt" (in "dirdata" folder)
     // outputs error message if data cannot be saved
-    if (file_put_contents('getEmployees.json', $jsondata))
+    if (file_put_contents('getEmployees.json', $jsondata)){
       echo 'Data successfully saved';
+      header("Location:http://localhost:8888/index.php");
+    }
     else
       echo 'Unable to save data in "getEmployees.json"';
   }
