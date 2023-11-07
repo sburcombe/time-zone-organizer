6065262d048037a098a200016f5facfa0153556a
Stephanie Burcombe <sburcombe@gmail.com>
2023-11-06 19:19:49 -0800

    adjusting the text for the dropdown menus

--- scheduleMeeting.php
+++ scheduleMeeting.php
@@ -22,12 +22,12 @@
         <input name="meeting-attendees" id="meeting-attendees" class="form-control">
         <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
+    Attendees
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
+    <a class="dropdown-item" href="#">Name 1</a>
+    <a class="dropdown-item" href="#">Name 2</a>
+    <a class="dropdown-item" href="#">Name 3</a>
   </div>
 </div>
       </input>
